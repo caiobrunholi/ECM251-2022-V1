afb3ac6d3d5a08dd0fb02adb47ce346ccfa1f974
caiobrunholi <caio.brunholi@hotmail.com>
2022-03-24 11:00:50 -0300

    fim da aula

--- MauaBank/L1/Projeto_Aula06/src/Sistema.java
+++ MauaBank/L1/Projeto_Aula06/src/Sistema.java
@@ -1,7 +1,30 @@
+import java.lang.invoke.VolatileCallSite;
+import java.time.LocalDate;
+
+
 public class Sistema {
     public void run(){
         Cliente cliente = new Cliente("Midoria", "123456", "allmight_wannabe@gmail.com");
         Conta conta = new Conta(cliente, 1234);
         System.out.println(conta);
+        
+        Titulo steam = new Titulo(200, LocalDate.of(2022,03,30), 5);
+
+        conta.depositar(300);
+
+
+    }
+
+    boolean pagarTitulo(Titulo titulo, Conta conta){
+        double valorPagar;
+        LocalDate dataTitulo = titulo.getData();
+        LocalDate hoje = LocalDate.now();
+        if(dataTitulo.compareTo(hoje) > 0){
+            valorPagar = titulo.getValor();
+        }else{
+            //To do: terminar implementação
+        }
+
+        return true;
     }
 }
