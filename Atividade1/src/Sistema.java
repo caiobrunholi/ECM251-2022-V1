00526b7219dd35984d9f343db8398fa20785fc0d
caiobrunholi <caio.brunholi@hotmail.com>
2022-04-28 11:01:03 -0300

    Implementações

--- Atividade1/src/Sistema.java
+++ Atividade1/src/Sistema.java
@@ -1,34 +1,40 @@
 public class Sistema {
     public void run(){
-        Veiculo carro = new Carro("carro", 1000);
+        Veiculo carro = new Carro("carro", 1000, "Honda", "NSX");
         Veiculo moto = new Moto("moto", 20000);
         Veiculo bicicleta = new Bicicleta("bicicleta", 30000);
         Veiculo patinete = new Patinete("patinete", 40000);
 
-        Usuario senna = new Usuario("Senna", 111111111, "airton@mclaren.com", carro);
-        Usuario mcqueen = new Usuario("McQueen", 22222222, "bullit@universal.com", moto);
-        Usuario eleanor = new Usuario("Eleanor", 333333333, "mustang@60seconds.com", bicicleta);
-        Usuario mate = new Usuario("Mate", 444444444, "towmatter@cars.com", patinete);
-        
-        // carro.testar();
-        // senna.veiculo.testar();
+        Usuario senna = new Usuario("Senna", 111111111, "airton@mclaren.com");
+        senna.pegarEmprestado(carro);
+        Usuario mcqueen = new Usuario("McQueen", 22222222, "bullit@universal.com");
+        mcqueen.pegarEmprestado(moto);
+        Usuario eleanor = new Usuario("Eleanor", 333333333, "mustang@60seconds.com");
+        eleanor.pegarEmprestado(bicicleta);
+        Usuario mate = new Usuario("Mate", 444444444, "towmatter@cars.com");
+        mate.pegarEmprestado(patinete);
 
         trocar(senna, mcqueen);
+        trocar(eleanor, mate);
     }
 
     public void trocar (Usuario u1, Usuario u2){
-        System.out.println("Troca entre: " + u1.getNome() + " e " + u2.getNome());
+        System.out.println("\nTroca entre: " + u1.getNome() + " e " + u2.getNome());
         System.out.println(u1);
         System.out.println(u2);
-        Veiculo veiculoU1 = u1.veiculo;
-        Veiculo veiculoU2 = u2.veiculo;
+        Veiculo veiculoU1 = u1.getVeiculo();
+        Veiculo veiculoU2 = u2.getVeiculo();
         u1.setVeiculo(veiculoU2);
         u2.setVeiculo(veiculoU1);
         System.out.println("Troca Realizada!");
         System.out.println(u1);
         System.out.println(u2);
-        u1.veiculo.testar();
-        u2.veiculo.testar();
+        veiculoU1 = u1.getVeiculo();
+        veiculoU2 = u2.getVeiculo();
+        System.out.println("Testar");
+        veiculoU1.testar();
+        veiculoU2.testar();
+        System.out.println("Pronto!\n");
     }
 
 }
