48f626152e24be102c07d8e5f7fb16b6d548e88b
caiobrunholi <caio.brunholi@hotmail.com>
2022-08-18 10:24:44 -0300

    Aula 15 - Intro POO com Python

---
+++ ProjetoLojaOnline/main.py
@@ -0,0 +1,34 @@
+from item import Item
+from carrinho import Carrinho
+
+item1=Item('Carregador','Carrega iPhone e Android',200.0)
+
+item2 = Item(
+    valor=350.0,
+    nome='Stray',
+    descricao='gato'
+)
+
+item3 = Item(
+    valor=350.0,
+    nome='Stray',
+    descricao='gato'
+)
+
+carrinho=Carrinho()
+print(f'Tamanho: {carrinho.get_quantidade_itens()}')
+print(f'Valor: {carrinho.get_valor_total()}')
+
+carrinho.adicionar(item1)
+carrinho.adicionar(item2)
+
+print(f'Tamanho: {carrinho.get_quantidade_itens()}')
+print(f'Valor: {carrinho.get_valor_total()}')
+
+# print(item1 == item2)
+# print(item1 == item1)
+# print(item2 == item3)
+
+carrinho.remover(item1)
+print(f'Tamanho: {carrinho.get_quantidade_itens()}')
+print(f'Valor: {carrinho.get_valor_total()}')
