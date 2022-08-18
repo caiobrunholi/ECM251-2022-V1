7a64ef61a2cba45981a0d616b53878740cba9b82
caiobrunholi <caio.brunholi@hotmail.com>
2022-08-18 11:01:12 -0300

    Criação do cartão de crédito

--- ProjetoLojaOnline/modelos/pagamentos/cartoes.py
+++ ProjetoLojaOnline/modelos/pagamentos/cartoes.py
@@ -21,3 +21,8 @@ class Debito(Cartao):
         return True
 
 
+class Credito(Cartao):
+    def __init__(self, numero, titular, validade, cvv):
+        super().__init__(numero, titular, validade, cvv)
+    def realizar_pagamento(self):
+        return True
