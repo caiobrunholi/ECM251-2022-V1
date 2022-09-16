7a497c2191e5e0f7b5ae211e180fabc3936deea4
caiobrunholi <caio.brunholi@hotmail.com>
2022-09-16 16:47:40 -0300

    implemtação da mudança de imagem de login

--- ProjetoLojaOnline_v02/src/pages/login.py
+++ ProjetoLojaOnline_v02/src/pages/login.py
@@ -1,13 +1,50 @@
-from cProfile import label
-from turtle import onclick
 import streamlit as st
+
+png_unknown = "assets./unknown_user.png"
+png_female = "assets./female_user.png"
+png_male = "assets./male_user.png"
+
+
+def update_profile_pic(png):
+    st.session_state["image"] = png
+    
 
 
 st.title("Login")
 st.write("Faça o Login para acessar a Lojinha Online!")
 
-user = st.text_input('Usuario', 'Username')
-password = st.text_input('Senha', 'Password')
+with st.container():
+    if "image" in st.session_state:
+        st.image(st.session_state["image"],width=100)
+    else:
+        st.image(png_unknown,width=100)
 
-loginbut = st.button("Login!")
+    user = st.text_input('Usuario', 'Username')
+    password = st.text_input('Senha', 'Password')
+
+    login_but = st.button(
+        label="Login", 
+        help="Login do Usuário",
+        on_click=update_profile_pic,
+        kwargs={"png":"assets./female_user.png"}
+    )
+
+    st.markdown("Não possui uma conta ainda?")
+    register_but= st.button(
+        label="Register", 
+        help="Registro de usuário",
+        on_click=update_profile_pic,
+        kwargs={"png":"assets./unknown_user.png"}
+    )
+
+if login_but:
+    st.write("Login Feito!")
+    
+if register_but:
+    st.write("Cadastrar usuário!")
+
+
+
+def somar_dois(png):
+    st.session_state["image"] = png
 
