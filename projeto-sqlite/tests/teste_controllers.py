3422ac565234e6f969bc72833c2524c13b15228f
caiobrunholi <caio.brunholi@hotmail.com>
2022-10-19 10:56:25 -0300

    fim da aula

---
+++ projeto-sqlite/tests/teste_controllers.py
@@ -0,0 +1,15 @@
+from src.controllers.item_controller import ItemController
+from src.models.item import Item
+
+controller = ItemController()
+items = controller.pegar_todos_itens()
+for item in items:
+    print(item)
+
+novo_item = Item("OLA1", "Cooler REDRAGON Vermelho", 19.90)
+print(controller.inserir_item(novo_item))
+
+print("**************************************")
+items = controller.pegar_todos_itens()
+for item in items:
+    print(item)
